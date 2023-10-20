26cc17fe1b40f4dc8b94da2aae104b828ae9490d
Eric Liu <ericl7552@gmail.com>
2023-10-20 12:06:35 -0400

    Initial Commit

---
+++ hw1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hw1
+//
+//  Created by eric liu on 2023-10-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hw1/hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  hw1App.swift
+//  hw1
+//
+//  Created by eric liu on 2023-10-20.
+//
+
+import SwiftUI
+
+@main
+struct hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
